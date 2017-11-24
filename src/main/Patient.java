bc6e03ad12d16f6b4c80dc117a94b5ccf26b9de1
BorosEdmund <borosedmund@gmail.com>
2017-11-24 20:39:49 +0200

    Patient class eqaul,toString method add

--- src/main/Patient.java
+++ src/main/Patient.java
@@ -3,17 +3,29 @@ package main;
 public class Patient {
 	
 	private String name;
-	private String complaint;
-	private int id;
+	private String complain;
+	public int nurse_id;
 	
 	public Patient(String name,String complain){
 		this.name = name;
-		this.complaint = complain;
+		this.complain = complain;
 	}
 	public String getComplain(){
-		return complaint;
+		return complain;
 	}
-	public String getName(){
-		return name;
+	public void setNurseId(int id){
+		nurse_id = id;
 	}
+	@Override
+	public String toString(){
+		return name + " " + complain + " " + nurse_id;
+	}
+	@Override
+	public boolean equals(Object s){
+		if(this.toString().hashCode() == s.toString().hashCode())
+			return true;
+		return false;	
+	}
+	
+ 
 }
