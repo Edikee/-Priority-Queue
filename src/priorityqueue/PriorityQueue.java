fd8ab5977c53f60ba91e04655f0560d640118c4c
BorosEdmund <borosedmund@gmail.com>
2017-11-24 19:00:24 +0200

    PriorityQueue class getMin, searchMi method done

--- src/priorityqueue/PriorityQueue.java
+++ src/priorityqueue/PriorityQueue.java
@@ -3,8 +3,10 @@ package priorityqueue;
 public class PriorityQueue<E> {
 	private List<E> list;
 	private List<Integer> priority;
-	private int priorityMinIndex;
+	private Integer priorityMinIndex;
 	private int priorityMin;
+	public boolean lock;
+	
 	public PriorityQueue(){
 		list = new List<E>();
 		priority = new List<Integer>();
@@ -26,9 +28,26 @@ public class PriorityQueue<E> {
 		
 		
 	}
+	public E getMin(){
+		E value = list.get(priorityMinIndex);
+		list.remove(value);
+		priority.remove(priorityMin);
+		searchMin();
+		return value;
+	}	
+	private void searchMin(){
+		priorityMin = Integer.MAX_VALUE;
+		for(int i=0 ; i< priority.length ; i++){
+			Integer tmp =  priority.get(i);
+			if(priorityMin > tmp){
+				priorityMin = tmp;
+				priorityMinIndex = i;
+			}
+		}
+	}
 	public void kiir(){
-		System.out.println(priorityMinIndex);
-		for(int i=0;i<priority.length;i++){
+ 
+		for(int i=0;i<list.length;i++){
 			System.out.println(list.get(i) + " " + priority.get(i)+ " ");
 		}
 	 
