6ee0db2f8c864857f0cd5bc1e1e5a6917989598d
BorosEdmund <borosedmund@gmail.com>
2017-11-24 23:13:59 +0200

    Something is wrong with prique

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,39 @@
 package main;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 
+		Nurse nurse = new Nurse();
+		Doctor doctor = new Doctor();
+ 
+		nurse.record(new Patient("a","cut")); 
+		nurse.record(new Patient("a","broken")); 
+		nurse.record(new Patient("a","stab")); 
+		nurse.record(new Patient("a","cut")); 
+		nurse.record(new Patient("a","cut")); 
+		nurse.record(new Patient("a","broken")); 
+		nurse.record(new Patient("a","stab")); 
+		nurse.record(new Patient("aa","shot")); 
+		nurse.record(new Patient("a","broken")); 
+		nurse.record(new Patient("a","cut")); 
+		nurse.record(new Patient("a","broken")); 
+		nurse.record(new Patient("a","stab")); 
+		nurse.record(new Patient("aa","shot")); 
+		nurse.record(new Patient("a","stab")); 
+		nurse.record(new Patient("aa","shot")); 
+		nurse.record(new Patient("aa","shot")); 
+		
+		for(int i=0;i<5;i++){
+			System.out.println(nurse.getPatient());
+		}
 
+		
 	}
+	 
 
 }
+/*
+		*/
--- src/main/Nurse.java
+++ src/main/Nurse.java
@@ -1,4 +1,6 @@
 package main;
+import javax.swing.plaf.synth.SynthSplitPaneUI;
+
 import priorityqueue.*;
 public class Nurse {
 	private PriorityQueue<Patient> prque;
@@ -9,20 +11,29 @@ public class Nurse {
 	}
 	public void record(Patient patient){
 		patient.setrecordId(recordId);
-		int priority = 0;
+		int priority = 30;
 		switch(patient.getComplain()){
-		case "shot": priority = 1;break;
-		case "broken": priority = 3;break;
-		case "cut": priority = 4;break;
-		case "stab": priority = 2;break;
+			case "shot": priority = 3;break;
+			case "broken": priority = 10;break;
+			case "cut": priority = 20;break;
+			case "stab": priority = 5;break;
 		}
+	 
 		prque.add(patient,priority);
 		recordId++;
 	}
 	public Patient getPatient(){
 		return prque.getMin();
 	}
+	public void kiir(){
+		prque.kiir();
+	}
 	public void event(Patient patient, String event){
-		
+		int newPriority = 0;
+		switch(event){
+			case "collapse" : newPriority = 2;break;
+			case "stop breath" : newPriority = 1;break;
+		}
+		prque.changePriority(patient, newPriority);
 	}
 }
