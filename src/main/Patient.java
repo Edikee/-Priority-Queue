51c0af2c0f8cc16446d71622e50ec6801c51baaa
BorosEdmund <borosedmund@gmail.com>
2017-11-24 20:31:31 +0200

    Patient class add

--- src/main/Patient.java
+++ src/main/Patient.java
@@ -1,5 +1,19 @@
 package main;
 
 public class Patient {
-
+	
+	private String name;
+	private String complaint;
+	private int id;
+	
+	public Patient(String name,String complain){
+		this.name = name;
+		this.complaint = complain;
+	}
+	public String getComplain(){
+		return complaint;
+	}
+	public String getName(){
+		return name;
+	}
 }
