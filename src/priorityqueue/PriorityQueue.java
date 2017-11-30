008188b08dba8aac4f9905051360c01e0dc6eeb8
BorosEdmund <borosedmund@gmail.com>
2017-11-30 14:40:18 +0200

    threadsafe

--- src/priorityqueue/PriorityQueue.java
+++ src/priorityqueue/PriorityQueue.java
@@ -5,6 +5,7 @@ public class PriorityQueue<E> extends AbstractQueue<E> {
 	protected List<Integer> priority;
 	private Integer priorityMinIndex;
 	private int priorityMin;
+	private Object lock = new Object();
 
 	public PriorityQueue() {
 		super();
@@ -20,34 +21,37 @@ public class PriorityQueue<E> extends AbstractQueue<E> {
 		return add(obj, 1);
 	}
 
-	public synchronized boolean add(E obj, Integer priorityNumber) { // O(1)
+	public boolean add(E obj, Integer priorityNumber) { // O(1)
+		synchronized (lock) {
 
-		list.add(obj);
-		priority.add(priorityNumber);
+			list.add(obj);
+			priority.add(priorityNumber);
 
-		if (priorityMin > priorityNumber) {
-			priorityMinIndex = 0;
-			priorityMin = priorityNumber;
-		} else {
-			priorityMinIndex++;
+			if (priorityMin > priorityNumber) {
+				priorityMinIndex = 0;
+				priorityMin = priorityNumber;
+			} else {
+				priorityMinIndex++;
+			}
 		}
 		return true;
 	}
 
 	@Override
-	public synchronized E getFirst() { // O(n)
+	public E getFirst() { // O(n)
+		E value = null;
+		synchronized (lock) {
+			value = list.get(priorityMinIndex);
 
-		E value = list.get(priorityMinIndex);
-
-		list.remove(value);
-		priority.removeIndexOf(priorityMinIndex);
-
-		searchMin();
+			list.remove(value);
+			priority.removeIndexOf(priorityMinIndex);
+			searchMin();
+		}
 
 		return value;
 	}
 
-	public synchronized void searchMin() { // O(n)
+	private void searchMin() { // O(n)
 		priorityMin = Integer.MAX_VALUE;
 
 		for (int i = 0; i < priority.size(); i++) {
@@ -62,12 +66,15 @@ public class PriorityQueue<E> extends AbstractQueue<E> {
 
 	}
 
-	public synchronized void changePriority(E oldObj, E newObj, int priorit) { // O(n)
-		int index = list.indexOf(oldObj);
-		if (list.remove(oldObj)) {
-			priority.removeIndexOf(index);
-			searchMin();
-			this.add(newObj, priorit);
+	public void changePriority(E oldObj, E newObj, int priorit) { // O(n)
+
+		synchronized (lock) {
+			int index = list.indexOf(oldObj);
+			if (list.remove(oldObj)) {
+				priority.removeIndexOf(index);
+				searchMin();
+				this.add(newObj, priorit);
+			}
 		}
 	}
 
