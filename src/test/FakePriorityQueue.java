900e65dda1bcf317d1fbda76577e333fac33c8dd
BorosEdmund <borosedmund@gmail.com>
2017-11-30 16:12:14 +0200

    threadtest remove

--- src/test/FakePriorityQueue.java
+++ src/test/FakePriorityQueue.java
@@ -5,13 +5,13 @@ import priorityqueue.*;
 public class FakePriorityQueue extends PriorityQueue<Node> {
 	public FakePriorityQueue() {
 		super();
-		Node a,b ;
+		Node a, b;
 		a = new Node();
 		a.setContain(6);
 		b = new Node();
 		b.setContain(5);
-		add(a,5);
-		add(b,6);
+		add(a, 5);
+		add(b, 6);
 	}
 
 }
